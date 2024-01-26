c4f02926a9c6aca29355849330c63897d0910b43
AoriIKA <44740781+AoriIKA@users.noreply.github.com>
2024-01-26 22:50:51 +0900

    音による三段ジャンプと音の大きさによるジャンプ力の変化を作成

--- Assets/ASAE/Script/PlayerMicrophoneController.cs
+++ Assets/ASAE/Script/PlayerMicrophoneController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerMicrophoneController : MonoBehaviour
 {
@@ -13,9 +14,19 @@ public class PlayerMicrophoneController : MonoBehaviour
     [SerializeField] private GameObject m_Cube;
     [SerializeField, Range(10, 100)] private float m_AmpGain = 10;
 
+    private Rigidbody palyerRigid;
+
+    private int playerJumpcount = 0;
+    private int playerMaxjumpCount = 3;
+
+    [SerializeField]
+    private bool isPlayOneshot = true;//一回だけ処理する際に都度更新する
+    private bool isFlagCache = false;
+
     //マイクデバイスを設定語オーディオクリップに反映
     void Start()
     {
+        palyerRigid = this.GetComponent<Rigidbody>();
         string targetDevice = "";
 
         foreach (var device in Microphone.devices)
@@ -37,14 +48,59 @@ public class PlayerMicrophoneController : MonoBehaviour
         PlayerSpeakingJanp();
     }
 
+    private void FixedUpdate()
+    {
+       // if (Input.GetKeyDown(KeyCode.K)) palyerRigid.AddForce(transform.up * (3 + m_AmpGain * m_AudioLevel), ForceMode.Impulse);
+    }
+
     void PlayerSpeakingJanp()
     {
         float[] waveData = GetUpdatedAudio();
         if (waveData.Length == 0) return;
 
         m_AudioLevel = waveData.Average(Mathf.Abs);
-        m_Cube.transform.localScale = new Vector3(1, 1 + m_AmpGain * m_AudioLevel, 1);
-        Debug.Log(1 + m_AmpGain * m_AudioLevel);
+
+        if (isFlagCache == isPlayOneshot) return;//フラグに差異がない場合はここで処理を終了する
+        
+        //一定の音量の強さからジャンプする
+        if(1 + m_AmpGain * m_AudioLevel >= 1.6f)
+        {
+            Debug.Log("Jump!");
+            //次のフレームで実行されないようにキャッシュを更新
+            palyerRigid.AddForce(transform.up * (5 + m_AmpGain * m_AudioLevel), ForceMode.Impulse);
+            isFlagCache = !isFlagCache;
+            if (playerJumpcount < playerMaxjumpCount-1)
+            {
+                Invoke("ResetFlag", 0.3f);
+                playerJumpcount++;
+            }
+
+        }
+        else
+        {
+            m_Cube.transform.localScale = new Vector3(1, 1 + m_AmpGain * m_AudioLevel, 1);
+            Debug.Log(1 + m_AmpGain * m_AudioLevel);
+        }
+        
+       
+
+       
+    }
+
+    private void ResetFlag()
+    {
+        
+        isPlayOneshot = !isPlayOneshot;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+        if (collision.gameObject.tag == "Ground" && playerJumpcount > 0)
+        {
+            playerJumpcount = 0;
+            isPlayOneshot = !isPlayOneshot;
+        }
     }
 
     private float[] GetUpdatedAudio()
