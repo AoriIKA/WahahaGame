44b15678a4a5360550b9fbd889a6e1d197723e33
marbas <ikenokami0930@gmail.com>
2024-01-28 09:36:02 +0900

    トランジション実装

---
+++ Assets/marbas/script/fadeIn.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class fadeIn : MonoBehaviour
+{
+    [SerializeField] Fade fade;
+    float time = 1f;
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void NextScene()
+    {
+        StartCoroutine(scene_change());
+        //1秒間フェードしてからにシーン移動する
+    }
+    IEnumerator scene_change()
+    {
+
+        fade.FadeIn(time);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Test_marbas_2");
+        fade.FadeOut(time);
+    }
+}
